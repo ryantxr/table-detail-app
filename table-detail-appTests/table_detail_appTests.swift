e3d960c8f1d0cf3d74e5f40c3d55f45ea1d4e352
ryantxr <ryantxr@gmail.com>
2016-03-21 11:19:02 -0400

    Initial Commit

---
+++ table-detail-appTests/table_detail_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  table_detail_appTests.swift
+//  table-detail-appTests
+//
+//  Created by ryan teixeira on 3/21/16.
+//  Copyright © 2016 Ryan Teixeira. All rights reserved.
+//
+
+import XCTest
+@testable import table_detail_app
+
+class table_detail_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
